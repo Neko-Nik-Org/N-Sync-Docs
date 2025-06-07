1cc267b5ea89f19f76af6b7f9d285ab273aeddb8
Neko Nik <nikhil@nekonik.com>
2025-06-07 22:46:57 +0530

    Initial Setup

--- README.md
+++ README.md
@@ -1,2 +1,85 @@
 # N-Ops-Docs
+
 Documentation Web For N-Ops Project
+
+Maintain the following directory structure for the documentation:
+
+```sh
+src/content/docs/
+├── backend
+│   ├── ansible.md
+│   ├── api-routes.md
+│   ├── models.md
+│   ├── overview.md
+│   └── security.md
+├── contribute
+│   ├── how.md
+│   ├── roadmap.md
+│   └── structure.md
+├── deployment
+│   ├── docker.md
+│   ├── local-dev.md
+│   └── production.md
+├── features
+│   ├── command-center.md
+│   ├── secure-notes.md
+│   └── sql-runner.md
+├── frontend
+│   ├── components.md
+│   ├── env-setup.md
+│   ├── overview.md
+│   ├── state-auth.md
+│   └── websockets.md
+├── get-started.md
+├── guides
+│   ├── example.md
+│   ├── installation.md
+│   ├── project-structure.md
+│   ├── what.md
+│   └── why.md
+└── index.mdx
+
+7 directories, 26 files
+```
+
+# Website
+
+This website is built using [Docusaurus](https://docusaurus.io/), a modern static website generator.
+
+## Installation
+
+```bash
+yarn
+```
+
+## Local Development
+
+```bash
+yarn start
+```
+
+This command starts a local development server and opens up a browser window. Most changes are reflected live without having to restart the server.
+
+## Build
+
+```bash
+yarn build
+```
+
+This command generates static content into the `build` directory and can be served using any static contents hosting service.
+
+## Deployment
+
+Using SSH:
+
+```bash
+USE_SSH=true yarn deploy
+```
+
+Not using SSH:
+
+```bash
+GIT_USER=<Your GitHub username> yarn deploy
+```
+
+If you are using GitHub pages for hosting, this command is a convenient way to build the website and push to the `gh-pages` branch.
---
+++ docusaurus.config.js
@@ -0,0 +1,162 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'N-Ops',
+  tagline: 'Automate your DevOps with N-Ops',
+  favicon: 'img/favicon.svg',
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: 'https://n-ops.nekonik.org',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Neko-Nik-Org', // Usually your GitHub org/user name.
+  projectName: 'N-Ops-Docs', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'N-Ops',
+        logo: {
+          alt: 'N Ops Logo',
+          src: 'img/favicon.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          // {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/Neko-Nik-Org/N-Ops-API',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Contact',
+            items: [
+              {
+                label: 'nikhil@nekonik.org',
+                href: 'mailto:nikhil@nekonik.org',
+              },
+              {
+                html: // For display the PO Box address
+                  `<div style="text-align: left; padding: 0; margin: 0;">
+                    <b>PO Box Address:</b>
+                    <p style="margin: 0; padding: 0; padding-left: 1rem">ePostbook Postbox - 86007</p>
+                    <p style="margin: 0; padding: 0; padding-left: 1rem">4/45 B, Grand Trunk Rd,</p>
+                    <p style="margin: 0; padding: 0; padding-left: 1rem">Shri Ram Nagar, Main Shahdara,</p>
+                    <p style="margin: 0; padding: 0; padding-left: 1rem">New Delhi, India - 110032</p>
+                  </div>`
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Discord',
+                href: 'https://discord.com/invite/PYqHVUGdwv',
+              },
+              // {
+              //   label: 'X',
+              //   href: 'https://x.com/NekoNikOrg',
+              // },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              // {
+              //   label: 'Blog',
+              //   to: '/blog',
+              // },
+              {
+                label: 'GitHub Organization',
+                href: 'https://github.com/Neko-Nik-Org',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Neko Nik. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
